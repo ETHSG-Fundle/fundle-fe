249468287f4bfd840b90195c2b914a0627d06d54
matthew-chua <mattchua2603@gmail.com>
2023-09-10 15:11:40 +0800

    register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,23 +5,8 @@ import WideCard from "@/components/StrategyCard";
 import React, { useState } from "react";
 
 export default function Page() {
-  const [activeTab, setActiveTab] = useState(0);
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <Tabs
-        label1="TEST 1"
-        label2="TEST 2"
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
-      {activeTab === 0 ? "TEST 1" : "TEST 2"}
-      <WideCard
-        title="Title"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-        stat1="10%"
-        stat2="10000%"
-        bigStat="1.9%"
-      />
-    </div>
-  );
+  return <div className="flex flex-col items-center">
+    <h1>Register Page</h1>
+    <p>This page allow beneficiaries to register themselves to receive funding using account abstraction.</p>
+  </div>;
 }
