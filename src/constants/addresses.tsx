2ab1f78b5008f9e4a4fbf56cb1d997be8fd7019f
matthew-chua <mattchua2603@gmail.com>
2023-09-11 16:45:30 +0800

    updated addresses

--- src/constants/addresses.tsx
+++ src/constants/addresses.tsx
@@ -1,9 +1,9 @@
 export const addresses = {
-  donationManager: "0x27aB44cA2bdEE4567050Ebb739691739C8149f03",
+  donationManager: "0x32f972DFbFAD84c986Db30681f1177e221ef72dd",
   daiContract: "0x4023Ec52F26Cb9093D642cd7e40751EeA304eAbe",
   sdaiContract: "0xaEcB1B62E8C3e6d0DeD2706c0e3A41b29B3Fdb73",
   mantleRelayerContract: "0x9a72c125b30f7bebE4B3e8D1cFFA214EEaf79128",
   mantleAusdcContract: "0x254d06f33bDc5b8ee05b2ea472107E300226659A",
   clonedDaiContract: "0x4023Ec52F26Cb9093D642cd7e40751EeA304eAbe",
-  sdaiStrategyContract: "0x7f100Bf3E3E8D701EC5E99490F9cd1D3ED2369bC",
+  sdaiStrategyContract: "0xea0F45be21af60bBb1321F960D381853b283744b",
 };
